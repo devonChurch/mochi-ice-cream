4f4851b6bba87f7433469e2ba2496c36cda7cf19
Devon Church <notifydevon@gmail.com>
2020-08-21 15:45:15 +1200

    feat: 🎸 add generic AngularJS application

---
+++ app-one/src/index.ts
@@ -0,0 +1,9 @@
+import * as angular from "angular";
+
+angular.module("appOne", []).component("wrapper", {
+  template: `<h1 class="h3">App One</h1>`,
+  controllerAs: "vm",
+  controller() {
+    const vm = this;
+  },
+});
---
+++ app-one/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const DIR_SRC = path.resolve(__dirname, "src");
+
+module.exports = {
+  entry: "./src/index",
+
+  mode: "development",
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+        include: [DIR_SRC]
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(DIR_SRC, "index.html")
+    })
+  ]
+};
