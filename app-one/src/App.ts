f1b8c7f8dfd363baf3534e4d81b4a02e5a5f5645
Devon Church <notifydevon@gmail.com>
2020-08-25 13:27:51 +1200

    feat: 🎸 create fictitious AngularJS flag async toggle

--- app-one/src/App.ts
+++ app-one/src/App.ts
@@ -1,5 +1,9 @@
 import * as angular from "angular";
 
+const checkFeature = () => new Promise((resolve) => setTimeout(() => {
+    resolve(true);
+}, 3000))
+
 angular.module("appOne", []).component("wrapper", {
     template: `
     <div class="alert alert-info">
@@ -7,12 +11,27 @@ angular.module("appOne", []).component("wrapper", {
       <ul>
         <li>Environment: <strong>{{ vm.mode }}</strong></li>
         <li>Framework: <strong>AngularJS</strong></li>
+        <li>Has Feature:
+            <strong>
+                <span ng-if="vm.hasFeature">Yes 👍</span>    
+                <span ng-if="!vm.hasFeature">No 👎</span>    
+            </strong>
+        </li>
       </ul>
     </div>
     `,
     controllerAs: "vm",
-    controller() {
+    controller($scope) {
       const vm = this;
       vm.mode = process.env.MODE;
+      vm.hasFeature = false;
+      vm.$onInit = function () {
+          console.log('Angular mounting!', vm, $scope);
+          checkFeature().then(response => {
+              console.log('Has result!', response);
+              vm.hasFeature = response;
+              $scope.$digest();
+          })
+      };
     },
 });
