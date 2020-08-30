85408508280eefac0fb71059c995211d285c35ed
Devon Church <notifydevon@gmail.com>
2020-08-30 13:22:16 +1200

    fix: 🐛 add nested AngularJS --> AngularJS Parcel scenario

--- app-two/src/App.ts
+++ app-two/src/App.ts
@@ -19,6 +19,16 @@ angular.module("appTwo", []).component("wrapper", {
     const vm = this;
     vm.applicationMode = process.env.MODE;
     vm.$onInit = () => setTimeout(async () => {
+      // AngularJS, by default, does not load nested AngularJS Parcel inside of
+      // an already Bootstrapped AngularJS application. There is an unsupported
+      // solution (see below) that works in this smaller scoped context and may
+      // prove useful in a lager scale implementations.
+      // 
+      // @see Angular Error | https://code.angularjs.org/1.5.8/docs/error/ng/btstrpd?p0=%26lt;div%20id%3D%22randomId%22%26gt;
+      // @see SingleSPA Issue | https://github.com/single-spa/single-spa-angularjs/issues/49
+      // @see Solution | https://www.linkedin.com/pulse/inception-one-angularjs-application-inside-another-m%C3%A1rquez-soto/
+      $element.data("$injector", "");
+
       const parcelContainer = $element[0].querySelector(`[data-parcel="app-three"]`);
       const domElement = document.createElement("div");
       
